2f2b726d789b85fd543bc3003727277765c5bf4b
YagoMaia <yagohenriquev123@gmail.com>
2024-02-08 22:00:14 -0300

    Adicionando funcionalidades para ter a lista de mangas atuais lendo

--- manga_dex.py
+++ manga_dex.py
@@ -2,6 +2,59 @@ import requests
 import os
 import alive_progress 
 import sys
+import json
+
+base_url = "https://api.mangadex.org"
+path_atual = os.path.dirname(os.path.realpath(__file__))
+
+languages =['pt-br']
+
+#* Mangas no Json Mangas Diários
+
+def json_mangas_diarios():
+    
+    if os.path.exists(f"{path_atual}/mangas_diarios.json"):
+        with open(f"{path_atual}/mangas_diarios.json", "r") as mangas_diarios:
+            data = json.load(mangas_diarios)
+    else:
+        data = {"Atuais":[]}
+        with open(f"{path_atual}/mangas_diarios.json", "w+") as mangas_diarios:
+            json.dump(data, mangas_diarios)
+    return data
+
+def adicionar_manga_diario(data:dict):
+    
+    repetido = False
+    
+    mangas_atuais = json_mangas_diarios()
+    for m in mangas_atuais['Atuais']:
+        if m['Titulo'] == data['Titulo']:
+            repetido = True
+    if repetido == False:
+        mangas_atuais['Atuais'].append(data)
+        
+        with open(f"{path_atual}/mangas_diarios.json", "w") as ponteiro_json:
+            json.dump(mangas_atuais, ponteiro_json)
+        print(f"\nMangá {data['Titulo']} adicionado na lista de mangás Atuais")
+    else:
+        print("\nMangá repetido")
+
+def remover_manga_diario(indice : int):
+    
+    mangas_atuais = json_mangas_diarios()
+    mangas_atuais['Atuais'].pop(indice)
+    with open(f"{path_atual}/mangas_diarios.json", "w") as ponteiro_json:
+        json.dump(mangas_atuais, ponteiro_json)
+    print("\nMangá removido")
+        
+def listar_mangas_diarios():
+    
+    mangas_atuais = json_mangas_diarios()
+    print("\nMangas na lista de atuais\n")
+    for index, m in enumerate(mangas_atuais['Atuais']):
+        print(f"{index+1} - {m['Titulo']}")
+
+#* Funções para baixar capitulos e mostrar mangás disponíveis
 
 def remover_repetidos(capitulos:list):
     capitulos_vistos = set()
@@ -43,87 +96,111 @@ def baixar_capitulos(capitulos, inicio, fim):
 
         with alive_progress.alive_bar(len(data_saver), title = f"Capitulo {num_chap} - Vol {vol_chap}") as bar:
             for index, page in enumerate(data_saver):
-                if os.path.exists(f"{folder_path}/Page {index}"):
-                    continue
-                r = requests.get(f"{host}/data/{chapter_hash}/{page}")
-                if r.status_code == 404:
-                    page = data[index]
-                    r = requests.get(f"{host}/data/{chapter_hash}/{page}")
-                with open(f"{folder_path}/Page {index}", mode="wb") as f:
-                    f.write(r.content)
+                if not os.path.exists(f"{folder_path}/Page {index}"):
+                    r = requests.get(f"{host}/data-saver/{chapter_hash}/{page}")
+                    if r.status_code == 404:
+                        page = data[index]
+                        r = requests.get(f"{host}/data/{chapter_hash}/{page}")
+                    with open(f"{folder_path}/Page {index}", mode="wb") as f:
+                        f.write(r.content)
                 bar()
             #print("Cápitulo já existente")
 
-base_url = "https://api.mangadex.org"
+def listar_mangas(titulo_manga):
+    r = requests.get(f"{base_url}/manga", params={"title": titulo_manga})
 
-languages =['pt-br']
+    mangas_achados = [manga for manga in r.json()["data"]]
 
-print("\nIniciando conexão com MangaDex\n")
+    if len(mangas_achados) != 0:
+
+        for index, m in enumerate(mangas_achados):
+            title = m["attributes"]['title']['en']
+            print(f"[{index + 1}] - {title}")
+
+        resp = int(input("\nQual mangá deseja selecionar? ")) - 1
+        
+        titulo_manga_selecionado = mangas_achados[resp]['attributes']['title']['en']
+        
+        return mangas_achados[resp], f"\n{titulo_manga_selecionado} selecionado"
+    return None, "\nMangá não encontrado"
 
 title = sys.argv[1]
 
-r = requests.get(
-    f"{base_url}/manga",
-    params={"title": title}
-)
-
-mangas_achados = [manga for manga in r.json()["data"]]
-
-if len(mangas_achados) != 0:
-
-    for index, m in enumerate(mangas_achados):
-        title = m["attributes"]['title']['en']
-        print(f"[{index + 1}] - {title}")
-
-    resp = int(input("\nQual mangá deseja selecionar? ")) - 1
-    print("\n")
+match title:
+    case "diario":
+        a = json_mangas_diarios()
     
-    manga_id = mangas_achados[resp]['id']
-    nome_manga = mangas_achados[resp]["attributes"]['title']['en']
+    case "adicionar":
+        nome_manga = sys.argv[2]
+        manga_selecionado, mensagem = listar_mangas(nome_manga)
+        print(mensagem)
+        if manga_selecionado is not None:
+            manga_id = manga_selecionado['id']
+            nome_manga = manga_selecionado["attributes"]['title']['en']
+            novo_manga = {'Titulo':nome_manga, 'Id': manga_id}
+            adicionar_manga_diario(novo_manga)
+    
+    case "remover":
+        numero_manga = int(sys.argv[2])
+        remover_manga_diario(numero_manga - 1)
+        
+    case "listar":
+        listar_mangas_diarios()
+    
+    case _:
+        print("\nIniciando conexão com MangaDex\n")
+        manga_selecionado, mensagem = listar_mangas(title)
+        print(mensagem)
+        if manga_selecionado is not None:
+        
+            manga_id = manga_selecionado['id']
+            nome_manga = manga_selecionado["attributes"]['title']['en']
+            # ultima_cap_add = manga_selecionado["attributes"]['attributes']['latestUploadedChapter']
+            
+            r1 = requests.get(f"{base_url}/manga/{manga_id}/feed", params={"translatedLanguage[]": languages})
 
-    r1 = requests.get(f"{base_url}/manga/{manga_id}/feed", params={"translatedLanguage[]": languages})
-
-    limit = r1.json()['total'] if r1.json()['total'] <= 500 else 500
+            limit = r1.json()['total'] if r1.json()['total'] <= 500 else 500
 
 
-    r2 = requests.get(
-        f"{base_url}/manga/{manga_id}/feed",
-        params={"translatedLanguage[]": languages, 'limit': limit},
-    )
+            r2 = requests.get(
+                f"{base_url}/manga/{manga_id}/feed",
+                params={"translatedLanguage[]": languages, 'limit': limit},
+            )
 
-    capitulos = []
+            capitulos = []
 
-    for manga in r2.json()['data']:
-        id_capitulo = manga["id"]
-        volume_capitulo = manga['attributes']["volume"]
-        n_capitulo = manga['attributes']["chapter"]
-        titulo_capitulo = manga['attributes']['title']
-        capitulos.append({'Num_Capitulo': n_capitulo, 'Id':id_capitulo, 'Titulo': titulo_capitulo, 'Volume':volume_capitulo})
+            for manga in r2.json()['data']:
+                id_capitulo = manga["id"]
+                volume_capitulo = manga['attributes']["volume"]
+                n_capitulo = manga['attributes']["chapter"]
+                titulo_capitulo = manga['attributes']['title']
+                capitulos.append({'Num_Capitulo': n_capitulo, 'Id':id_capitulo, 'Titulo': titulo_capitulo, 'Volume':volume_capitulo})
 
-    capitulos_organizados = sorted(capitulos, key=lambda x : float(x['Num_Capitulo']))
+            capitulos_organizados = sorted(capitulos, key=lambda x : float(x['Num_Capitulo']))
 
-    capitulos_sem_repeticao = remover_repetidos(capitulos_organizados)
+            capitulos_sem_repeticao = remover_repetidos(capitulos_organizados)
 
-    for index, cap_vol in enumerate(capitulos_sem_repeticao):
-        print(f"({index + 1}) - Capitulo {cap_vol['Num_Capitulo']} - {cap_vol['Titulo']}")
+            for index, cap_vol in enumerate(capitulos_sem_repeticao):
+                print(f"({index + 1}) - Capitulo {cap_vol['Num_Capitulo']} - {cap_vol['Titulo']}")
 
-    #* Ajustar para poder escolher os capitulos para baixar
+            #* Ajustar para poder escolher os capitulos para baixar
 
-    print(f"\n{len(capitulos_sem_repeticao)} Capitulos encontrados\n")
+            print(f"\n{len(capitulos_sem_repeticao)} Capitulos encontrados\n")
 
-    escolha_cap = input("Quais capitulos deseja baixar? ")
-
-    if "-" in escolha_cap:
-        cap_ini, cap_fim = escolha_cap.split("-")
-        baixar_capitulos(capitulos_sem_repeticao, int(cap_ini) - 1, int(cap_fim) - 1)
-    elif escolha_cap.isnumeric():
-        baixar_capitulos(capitulos_sem_repeticao, int(escolha_cap) - 1, int(escolha_cap) - 1)
-    elif escolha_cap == "todos":
-        baixar_capitulos(capitulos_sem_repeticao, 0, len(capitulos_sem_repeticao))
-    elif escolha_cap == "nenhum":
-        print("Nenhum capitulo será baixado")
-    else:
-        print("opção não existente")
-else:
-    print("Mangá não encontrado")
+            escolha_cap = input("Quais capitulos deseja baixar? ")
+            print("\n")
+            
+            if "-" in escolha_cap:
+                cap_ini, cap_fim = escolha_cap.split("-")
+                baixar_capitulos(capitulos_sem_repeticao, int(cap_ini) - 1, int(cap_fim) - 1)
+            elif escolha_cap.isnumeric():
+                baixar_capitulos(capitulos_sem_repeticao, int(escolha_cap) - 1, int(escolha_cap) - 1)
+            elif escolha_cap == "todos":
+                baixar_capitulos(capitulos_sem_repeticao, 0, len(capitulos_sem_repeticao))
+            elif escolha_cap == "nenhum":
+                print("Nenhum capitulo será baixado")
+            else:
+                print("opção não existente")
+        else:
+            print("Mangá não encontrado")
 
